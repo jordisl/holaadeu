525c2ffc9106b548ea88a7dc1a838f5ece6ff9c0
jordisl <72779969+jordisl@users.noreply.github.com>
2020-10-13 06:41:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-led.plotBrightness(2, 0, 103)
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hola")
+    basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("adeu")
+    basic.pause(100)
+    basic.showIcon(IconNames.Sad)
+})
+basic.clearScreen()
 basic.forever(function () {
 	
 })
